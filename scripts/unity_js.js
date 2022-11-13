95c1979f0c59ba804da36bea24d72a818f772ead
Ajas m m <46745732+Ajasmm@users.noreply.github.com>
2022-11-13 22:31:20 +0530

    final-from-home

--- scripts/unity_js.js
+++ scripts/unity_js.js
@@ -1,11 +1,65 @@
 $(document).ready(function () {
     $('#pray-btn').mouseenter(function () { 
-        document.getElementById("made-with-unity-img").src = "images/pray-of.webp";
+        document.getElementById("made-with-unity").style = "background-image: url('https://raw.githubusercontent.com/Ajasmm/Unity/main/images/pray-of.webp')";
+        document.getElementById("lg-pray-btn").style = "Display : block;"
+        document.getElementById("lg-pharos-btn").style = "Display : none;"
+        document.getElementById("lg-config-car-btn").style = "Display : none;"
+        document.getElementById("md-pray-btn").style = "background-color: #2196f3;"
+        document.getElementById("md-pharos-btn").style = "background-color: #8b98a4;"
+        document.getElementById("md-config-car-btn").style = "background-color: #8b98a4;"
     });
+    $('#md-pray-btn').click(function () { 
+        document.getElementById("made-with-unity").style = "background-image: url('https://raw.githubusercontent.com/Ajasmm/Unity/main/images/pray-of.webp')";
+        document.getElementById("lg-pray-btn").style = "Display : block;"
+        document.getElementById("lg-pharos-btn").style = "Display : none;"
+        document.getElementById("lg-config-car-btn").style = "Display : none;"
+        document.getElementById("md-pray-btn").style = "background-color: #2196f3;"
+        document.getElementById("md-pharos-btn").style = "background-color: #8b98a4;"
+        document.getElementById("md-config-car-btn").style = "background-color: #8b98a4;"
+    });
+
     $('#pharos-btn').mouseenter(function () { 
-        document.getElementById("made-with-unity-img").src = "images/pharos-ar-made-with-unity.jpg  ";
+        document.getElementById("made-with-unity").style = "background-image: url('https://raw.githubusercontent.com/Ajasmm/Unity/main/images/pharos-ar-made-with-unity.jpg')";
+        document.getElementById("lg-pray-btn").style = "Display : none;"
+        document.getElementById("lg-pharos-btn").style = "Display : block;"
+        document.getElementById("lg-config-car-btn").style = "Display : none;"
+        document.getElementById("md-pray-btn").style = "background-color: #8b98a4;"
+        document.getElementById("md-pharos-btn").style = "background-color: #2196f3;"
+        document.getElementById("md-config-car-btn").style = "background-color: #8b98a4;"
     });
+    $('#md-pharos-btn').click(function () { 
+        document.getElementById("made-with-unity").style = "background-image: url('https://raw.githubusercontent.com/Ajasmm/Unity/main/images/pharos-ar-made-with-unity.jpg')";
+        document.getElementById("lg-pray-btn").style = "Display : none;"
+        document.getElementById("lg-pharos-btn").style = "Display : block;"
+        document.getElementById("lg-config-car-btn").style = "Display : none;"
+        document.getElementById("md-pray-btn").style = "background-color: #8b98a4;"
+        document.getElementById("md-pharos-btn").style = "background-color: #2196f3;"
+        document.getElementById("md-config-car-btn").style = "background-color: #8b98a4;"
+    });
+
     $('#config-car-btn').mouseenter(function () { 
-        document.getElementById("made-with-unity-img").src = "images/auto-showcase-1920x1080@1x.jpg";
+        document.getElementById("made-with-unity").style = "background-image: url('https://raw.githubusercontent.com/Ajasmm/Unity/main/images/auto-showcase-1920x1080%401x.jpg')";
+        document.getElementById("lg-pray-btn").style = "Display : none;"
+        document.getElementById("lg-pharos-btn").style = "Display : none;"
+        document.getElementById("lg-config-car-btn").style = "Display : block;"
+        document.getElementById("md-pray-btn").style = "background-color: #8b98a4;"
+        document.getElementById("md-pharos-btn").style = "background-color: #8b98a4;"
+        document.getElementById("md-config-car-btn").style = "background-color: #2196f3;"
     });
+    $('#md-config-car-btn').click(function () { 
+        document.getElementById("made-with-unity").style = "background-image: url('https://raw.githubusercontent.com/Ajasmm/Unity/main/images/auto-showcase-1920x1080%401x.jpg')";
+        document.getElementById("lg-pray-btn").style = "Display : none;"
+        document.getElementById("lg-pharos-btn").style = "Display : none;"
+        document.getElementById("lg-config-car-btn").style = "Display : block;"
+        document.getElementById("md-pray-btn").style = "background-color: #8b98a4;"
+        document.getElementById("md-pharos-btn").style = "background-color: #8b98a4;"
+        document.getElementById("md-config-car-btn").style = "background-color: #2196f3;"
+    });
+    
 });
+
+document.onload = RestAll();
+function RestAll(){
+    document.getElementById("lg-pharos-btn").style = "Display : none;"
+    document.getElementById("lg-config-car-btn").style = "Display : none;"
+}
