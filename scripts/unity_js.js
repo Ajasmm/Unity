d478e9902f0b762225d3a1ea7898a63b9e81563a
Ajas m m <46745732+Ajasmm@users.noreply.github.com>
2022-11-13 20:51:34 +0530

    fooding-gap

---
+++ scripts/unity_js.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#pray-btn').mouseenter(function () { 
+        document.getElementById("made-with-unity-img").src = "images/pray-of.webp";
+    });
+    $('#pharos-btn').mouseenter(function () { 
+        document.getElementById("made-with-unity-img").src = "images/pharos-ar-made-with-unity.jpg  ";
+    });
+    $('#config-car-btn').mouseenter(function () { 
+        document.getElementById("made-with-unity-img").src = "images/auto-showcase-1920x1080@1x.jpg";
+    });
+});
